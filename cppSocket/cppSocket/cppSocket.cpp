a849786692ef122be6995fe42d695cc17d3d6549
mikleligin <mikleligin@gmail.com>
2024-07-04 19:24:35 +0300

    fixed answer and refactor of code

--- cppSocket/cppSocket/cppSocket.cpp
+++ cppSocket/cppSocket/cppSocket.cpp
@@ -19,7 +19,12 @@ void MsgHandler(int index)
     //std::cout << index << std::endl;
     while (true)
     {
-        recv(connections[index], msg, sizeof(msg), NULL);
+        int res = recv(connections[index], msg, sizeof(msg), NULL);
+        if (res <= 0) {
+            // Connection closed or error
+            closesocket(connections[index]);
+            return;
+        }
         for (size_t i = 0; i < connector; i++)
         {
             if (i == index)
@@ -34,7 +39,7 @@ void MsgHandler(int index)
         }
     }
 }
-int main(){
+int main() {
 
     // Запрашиваем библиотеку ws 
     WSADATA wsaData;
@@ -48,32 +53,41 @@ int main(){
     SOCKADDR_IN address;
     int size = sizeof(address);
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = htons(1234);
+    address.sin_port = htons(1233);
     address.sin_family = AF_INET;
 
     SOCKET listener = socket(AF_INET, SOCK_STREAM, NULL);
     // Связка сокета + лисенера
-    bind(listener, (SOCKADDR*)&address, size);
+    //bind(listener, (SOCKADDR*)&address, size);
+
+    if (bind(listener, (SOCKADDR*)&address, size) == SOCKET_ERROR) {
+        std::cerr << "Bind failed. Error: " << WSAGetLastError() << std::endl;
+        closesocket(listener);
+        return 1;
+    }
+
     listen(listener, SOMAXCONN);
     SOCKET newConnection;
     for (size_t i = 0; i < CONNECTIONS; i++)
     {
         int c = 0;
         newConnection = accept(listener, (SOCKADDR*)&address, &size);
-        if (newConnection == 0) {
-            std::cout << "Someone tryed to connect\n";
+        if (newConnection == INVALID_SOCKET) {
+            std::cerr << "Failed to accept connection. Error: " << WSAGetLastError() << std::endl;
+            continue;
         }
         else {
             char msg[256] = "Welcome to my server";
             send(newConnection, msg, sizeof(msg), NULL);
             std::cout << "Client connected!, here is IP:";
-            std::cout << (SOCKADDR*)&address.sin_addr.s_addr << std::endl;
+            std::cout << inet_ntoa(address.sin_addr) << std::endl;
             connections[i] = newConnection;
             connector++;
-            
-            CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MsgHandler, LPVOID(i), NULL, NULL);
+
+            std::thread(MsgHandler, i).detach();
         }
     }
+    WSACleanup();
     system("pause");
     return 0;
 }
